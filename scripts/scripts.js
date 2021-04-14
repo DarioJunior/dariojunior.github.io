c2148ec64a9415ab0b23937ec9b1e5b707d3f657
Dario Junior <dario.junior.rs@gmail.com>
2021-04-14 00:18:28 -0300

    to fazendo cagada, mexendo na main :(

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,11 @@
 let book = document.querySelector('.book');
 let modal = document.querySelector('#projectsModal');
 let close = document.querySelector('#close');
+let galleryModal = document.querySelector('#gallery');
 
 book.addEventListener('click', () => {
   modal.style.visibility = 'visible';
+
 })
 
 close.addEventListener('click', () => {
