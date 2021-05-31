4d98354aac051123e1374f6f98d0b581842ebf9b
Dario Junior <dario.junior.rs@gmail.com>
2021-05-31 10:47:18 -0300

    create stack container in home

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,11 +3,31 @@ import React from 'react';
 import styles from './Projects.module.css';
 
 export default class Projects extends React.Component {
+  
   render() {
     return (
       <div className={styles.container}>
-        Projects
-      </div>
+        <a href="#PROJECTS">
+          <div>
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="HTML"/> 
+          </div>
+        </a>
+        <a href="#PROJECTS">
+        <div>
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="CSS"/>  
+          </div>
+        </a>
+        <a href="#PROJECTS">
+          <div>
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="Javascript"/>
+          </div>
+        </a>
+        <a href="#PROJECTS">
+          <div>
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="ReactJS"/>
+          </div>
+        </a>
+      </div> 
     )
   }
 }
