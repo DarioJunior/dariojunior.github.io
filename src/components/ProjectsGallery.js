c76bda690cee58b55df25130ebe44df362b4c422
Dario Junior <dario.junior.rs@gmail.com>
2021-05-31 15:15:00 -0300

    fix: link dev_finance repo in gallery

--- src/components/ProjectsGallery.js
+++ src/components/ProjectsGallery.js
@@ -8,11 +8,11 @@ export default class ProjectsGallery extends React.Component {
   render() {
     return (
       <div id="PROJECTS" className={styles.container}>
-        {/* <a target="_blank" href={DevFinance}> */}
+        <a target="_blank" href='https://github.com/DarioJunior/dev-finance-app'>
           <div>
             <img src={Project1} alt="Dev Finance"/>
           </div>
-        {/* </a> */}
+        </a>
         {/* <div>
           Podcastr
         </div>
