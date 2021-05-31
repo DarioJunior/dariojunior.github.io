7af2ad11234a3548a758a428c25ffa7a43d1f64d
Dario Junior <dario.junior.rs@gmail.com>
2021-05-31 10:44:12 -0300

    Add projectsGallery and Contact Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import styles from './App.module.css';
 import Header from './components/Header';
 import Profile from './components/Profile';
 import Projects from './components/Projects';
+import ProjectsGallery from './components/ProjectsGallery';
+import Contact from './components/Contact';
 
 //LEMBRAR DO "homapage" no PACKAGEJSON 
 function App() {
   return (
-    <div className={styles.App}>
+    <div className={styles.App} id="HOME">
       <Header />
       <Profile />
       <Projects /> 
+      <ProjectsGallery  className={styles.ProjectsGallery}/>
+      <Contact />
     </div>
   );
 }
